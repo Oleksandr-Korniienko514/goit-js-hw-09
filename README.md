e13c9a6393620cc5709d637775011e469936a4d6
Oleksandr-Korniienko514 <bon.bon199993@gmail.com>
2024-07-23 01:53:17 +0200

    fix

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-
 # goit-js-hw-09
 
+https://ii-777.github.io/goit-js-hw-09/
--- src/1-gallery.js
+++ src/1-gallery.js
@@ -2,7 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 
-
+// Change code below this line
 
 const gallery = document.querySelector('.gallery');
 const list = galleryItems.map(
